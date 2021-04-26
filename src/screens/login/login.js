039465f4bb924b8fbb9382ee5d6a750e8c33beaa
Emmapr123 <emmapriester@hotmail.com>
2021-04-26 13:45:02 +0100

    ugh

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -3,17 +3,43 @@ import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-nativ
 import { Headbar } from '../../components/headbar'
 import axios from 'axios';
 
-function LogInScreen({ navigation }) {
+const LogInScreen = ({ navigation }) => {
+
+const [userName, setUserName] = useState('')
+const [passWord, setPassWord] = useState('')
+
+
+const createUser = async() => {
+  const fakeResponse = { "forename": '', "surname": ' ', "username": ' ', "email": ' ', "password": ' ', "profilePic": "" },
+
+    try{
+      const getRequest = await axios.get("https://acebook--backend.herokuapp.com/users")
+      if ( !getRequest.ok ) {
+        return fakeResponse
+      } else {
+        console.log({error})
+      }
+        // { "username": '', "password": '' })
+        navigation.navigate("Log In");
+    }
+    catch(error){
+      console.log('error', {error});
+    }
+  }
 
   return (
 <View>
 <Headbar/>
     <View style={styles.container}>
           <TextInput style={styles.textInput}
-            placeholder="email"
+            placeholder="username"
+            onChangeText={(text) => setUserName(text)}
+            value={userName}
           />
           <TextInput style={styles.textInput}
             placeholder="password"
+            onChangeText={(text) => setPassWord(text)}
+            value={passWord}
           />
           <View style={styles.buttonContainer}>
             <Button title="submit" style={styles.button}/>
